bf79f8465012599b39c8e32b699ce5a9c66c3051
theisaachome <65864160+theisaachome@users.noreply.github.com>
2022-06-08 23:05:18 +0630

    Configure Routes Using Properties

--- internet-banking-api-gateway/src/test/java/com/highwayfinance/InternetBankingApiGatewayApplicationTests.java
+++ internet-banking-api-gateway/src/test/java/com/highwayfinance/InternetBankingApiGatewayApplicationTests.java
@@ -1,12 +1,12 @@
 package com.highwayfinance;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class InternetBankingApiGatewayApplicationTests {
 
-	@Test
+//	@Test
 	void contextLoads() {
 	}
 
